b8515194bc2eac9bd9d9f48bc11d074aafc1f04b
pavelvasilev <pavelvasilev2304@gmail.com>
2022-05-02 12:40:28 -0400

    Inital

---
+++ server.py
@@ -0,0 +1,82 @@
+from flask import Flask, render_template, request, redirect, url_for
+import webbrowser
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "Tarantino_is_ugly"
+
+
+@app.route('/index/<name>')
+@app.route("/<name>")
+def main_page(name):
+    return render_template("main.html", TITLE=name)
+
+
+@app.route("/promotion")
+def promotion_page():
+    return   """<h1>Человечество вырастает из детства.</h1></br>
+                <h1>Человечеству мала одна планета.</h1></br>
+                <h1>Мы сделаем обитаемыми безжизненные пока планеты.</h1></br>
+                <h1>И начнем с Марса!</h1></br>
+                <h1>Присоединяйся!</h1></br>"""
+
+
+@app.route("/image_mars")
+def image():
+    return render_template("image_mars.html")
+
+
+@app.route("/promotion_image")
+def promotion_image():
+    return render_template("promotion_image.html")
+
+
+@app.route("/astronaut_selection", methods=['GET', 'POST'])
+def reaction():
+    if request.method == "GET":
+        return render_template("astro_selection.html")
+    elif request.method == "POST":
+        return redirect("https://lkml.org/", code=200)
+
+
+@app.route("/load_image", methods=["GET", "POST"])
+def load_photo():
+    if request.method == "GET":
+        return render_template("load_image.html")
+    if request.method == "POST":
+        f = request.files['file']
+        with open("./static/img/buffer.jpg", 'wb') as image:
+            image.write(f.read())
+        return render_template("answer_image.html")
+
+
+@app.route("/carousel", methods=["GET", "POST"])
+def carousel():
+    if request.method == "GET":
+        return render_template("carousel.html")
+
+
+@app.route("/choice/<planet>")
+def choice(planet):
+    return render_template("choice.html", planet_name=planet)
+
+
+@app.route("/results/<name>/<int:step>/<float:score>")
+def results(**keys):
+    return render_template("results.html", name=keys['name'], hello=keys['step'], score=keys['score'])
+
+
+@app.route("/training/<prof>")
+def training(prof):
+    if "строитель" in prof.lower() or "инженер" in prof.lower():
+        return render_template("training.html", TITLE="Инженерная специальность", prof="Инженерная", filepath=url_for('static', filename='/img/engineer.jpeg'))
+    return render_template("training.html", TITLE="Научная специальность", prof="Научная", filepath=url_for('static', filename='/img/scientist.jpeg'))
+
+
+@app.route("/list_prof/<outt>")
+def list_prof(outt):
+    list_ = ['Машиностроитель', 'Строитель', 'Конструктор', 'Учёный', 'Биолог']
+    return render_template("list_prof.html", value=outt, list=list_)
+
+
+webbrowser.open("http://127.0.0.1:8080/Машина")
+app.run(port=8080)
