ce1e0669c19b68fc840051d9a5d4ae9c7e794a30
pavelvasilev <pavelvasilev2304@gmail.com>
2022-05-06 13:19:00 -0400

    Added user's and job's ress

--- news_resources.py
+++ news_resources.py
@@ -4,13 +4,21 @@ from data import db_session
 from data.__all_models import *
 
 
+def abort_if_user_not_found(job_id):
+    s = db_session.create_session()
+    new = s.query(User).filter(User.id == job_id).first()
+    s.close()
+    if not new:
+        abort(404, message=f"User with id ({job_id}) not found.")
+
+
 def abort_if_job_not_found(job_id):
     s = db_session.create_session()
     new = s.query(Jobs).filter(Jobs.id == job_id).first()
     s.close()
     if not new:
         abort(404, message=f"Job with id ({job_id}) not found.")
-        
+
 
 parser = reqparse.RequestParser()
 parser.add_argument('team_leader', required=True, type=int)
@@ -20,7 +28,66 @@ parser.add_argument('collaborators', required=True, type=str)
 parser.add_argument('is_finished', required=True, type=bool)
 
 
+uparser = reqparse.RequestParser()
+uparser.add_argument('surname', required=True, type=str)
+uparser.add_argument('name', required=True, type=str)
+uparser.add_argument('age', required=True, type=int)
+uparser.add_argument('email', required=True, type=str)
+
+
 class UsersResource(Resource):
+    def get(self, job_id):
+        abort_if_user_not_found(job_id)
+        s = db_session.create_session()
+        job = s.query(User).get(job_id)
+        s.close()
+        return jsonify({
+            'user': {key: job.__dict__[key] 
+                    for key in job.__dict__.keys()
+                    if not key.startswith('_')}
+        })
+    
+    def delete(self, job_id):
+        abort_if_user_not_found(job_id)
+        s = db_session.create_session()
+        s.query(User).filter(User.id == job_id).delete()
+        s.commit()
+        s.close()
+        return jsonify({
+            'status': True
+        })
+
+
+class UsersListResource(Resource):
+    def get(self):
+        s = db_session.create_session()
+        jobs = s.query(User).all()
+        s.close()
+        return jsonify({
+            'users': [{key: job.__dict__[key] 
+                    for key in job.__dict__.keys()
+                    if not key.startswith('_')}
+                    for job in jobs]
+        })
+    
+    def post(self):
+        args = uparser.parse_args()
+        s = db_session.create_session()
+        job = User(
+            surname = args['surname'],
+            name = args['name'],
+            age = args['age'],
+            email = args['email'],
+        )
+        s.add(job)
+        s.commit()
+        s.close()
+        return jsonify({
+            'status': True
+        })
+
+
+class JobsResource(Resource):
     def get(self, job_id):
         abort_if_job_not_found(job_id)
         s = db_session.create_session()
@@ -34,6 +101,7 @@ class UsersResource(Resource):
     
     def delete(self, job_id):
         abort_if_job_not_found(job_id)
+        print("[!]")
         s = db_session.create_session()
         s.query(Jobs).filter(Jobs.id == job_id).delete()
         s.commit()
@@ -43,7 +111,7 @@ class UsersResource(Resource):
         })
 
 
-class UsersListResource(Resource):
+class JobsListResource(Resource):
     def get(self):
         s = db_session.create_session()
         jobs = s.query(Jobs).all()
@@ -78,6 +146,8 @@ app = Flask(__name__)
 api = Api(app)
 api.add_resource(UsersResource, '/api/v2/users/<int:job_id>')
 api.add_resource(UsersListResource, '/api/v2/users')
+api.add_resource(JobsResource, '/api/v2/jobs/<int:job_id>')
+api.add_resource(JobsListResource, '/api/v2/jobs')
 app.run(port=8080)
 
 
--- testing_api.py
+++ testing_api.py
@@ -4,10 +4,43 @@ import requests
 
 url_user = "http://127.0.0.1:8080/api/v2/users/{}"
 url_users = "http://127.0.0.1:8080/api/v2/users"
+url_job = "http://127.0.0.1:8080/api/v2/jobs/{}"
+url_jobs = "http://127.0.0.1:8080/api/v2/jobs"
 
 
 def test_create_user():
     response = requests.post(url_users,json={
+        'surname': "обычный",
+        'name': 'Инженер',
+        'age': 23,
+        'email': 'abc214@gm.com',
+    }).json()
+    assert response['status'] == 1
+
+
+def test_get_users():
+    response = requests.get(url_users).json()
+    assert len(response['users']) > 0
+
+
+def test_get_user():
+    response = requests.get(url_users).json()
+    id_ = response['users'][0]['id']
+    response2 = requests.get(url_user.format(id_)).json()
+    assert 'user' in response2
+
+
+def test_delete_corr():
+    response = requests.get(url_users).json()
+    length = len(response['users'])
+    id_ = response['users'][0]['id']
+    requests.delete(url_user.format(id_)).json()
+    new_length = len(requests.get(url_users).json()['users'])
+    assert length - new_length == 1
+
+
+def test_create_job():
+    response = requests.post(url_jobs,json={
         'team_leader': 10,
         'job': 'Инженер',
         'work_size': 24,
@@ -18,23 +51,23 @@ def test_create_user():
     assert response['status'] == 1
 
 
-def test_get_users():
-    response = requests.get(url_users).json()
+def test_get_jobs():
+    response = requests.get(url_jobs).json()
     assert len(response['jobs']) > 0
 
 
-def test_get_user():
-    response = requests.get(url_users).json()
+def test_get_job():
+    response = requests.get(url_jobs).json()
     id_ = response['jobs'][0]['id']
-    response2 = requests.get(url_user.format(id_)).json()
+    response2 = requests.get(url_job.format(id_)).json()
     assert 'job' in response2
 
 
-def test_delete_corr():
-    response = requests.get(url_users).json()
+def test_delete_corr_j():
+    response = requests.get(url_jobs).json()
     length = len(response['jobs'])
     id_ = response['jobs'][0]['id']
-    requests.delete(url_user.format(id_)).json()
-    new_length = len(requests.get(url_users).json()['jobs'])
+    requests.delete(url_job.format(id_)).json()
+    new_length = len(requests.get(url_jobs).json()['jobs'])
     assert length - new_length == 1
     
